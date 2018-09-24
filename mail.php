49fc6bce6b2197fad04b92d105085ec95d0ef8ea
MatheusMondenez <matheus.mondenez@superlogica.com>
2018-09-23 21:43:17 -0300

    .

--- mail.php
+++ mail.php
@@ -1,11 +1,8 @@
 <?php
-    $to = 'demo@spondonit.com';
-    $firstname = $_POST["fname"];
+    $to = 'matheusmondenez@gmail.com';
+    $name = $_POST["name"];
     $email= $_POST["email"];
     $text= $_POST["message"];
-    $phone= $_POST["phone"];
-    
-
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
@@ -13,19 +10,17 @@
 
     $message ='<table style="width:100%">
         <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
+            <td>'.$name.'</td>
         </tr>
         <tr><td>Email: '.$email.'</td></tr>
         <tr><td>phone: '.$phone.'</td></tr>
         <tr><td>Text: '.$text.'</td></tr>
-        
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
+    if (@mail($to, $email, $message, $headers)) {
+        echo 'Enviado com sucesso!';
+    } else {
+        echo 'Falhou!';
     }
 
 ?>
